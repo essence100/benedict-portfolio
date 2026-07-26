406d4a3c9de70922360b8d94e1faa79d64328bf8
essence100 <essencebenedict@gmail.com>
2026-07-27 00:46:41 +0300

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,1 +1,63 @@
+function Contact() {
 
+  return (
+    <section className="contact" id="contact">
+
+
+      <div className="section-title">
+        <h2>Contact Me</h2>
+      </div>
+
+
+      <div className="contact-content">
+
+        <p>
+          Have a project or need IT support?
+          Feel free to contact me.
+        </p>
+
+
+        <div className="contact-links">
+
+
+          <a href="mailto:essencebenedict@gmail.com">
+            Email
+          </a>
+
+
+          <a 
+            href="https://wa.me/255782722871"
+            target="_blank"
+          >
+            WhatsApp
+          </a>
+
+
+          <a 
+            href="https://instagram.com/benedict_charse"
+            target="_blank"
+          >
+            Instagram
+          </a>
+
+
+          <a 
+            href="https://github.com/"
+            target="_blank"
+          >
+            GitHub
+          </a>
+
+
+        </div>
+
+
+      </div>
+
+
+    </section>
+  );
+}
+
+
+export default Contact;
