2871cf403ade91302fad7df67637137ccc27a1c2
essence100 <essencebenedict@gmail.com>
2026-07-27 00:39:36 +0300

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,1 +1,45 @@
+function Navbar() {
 
+  return (
+    <nav className="navbar">
+
+      <div className="logo">
+        B.E.C
+      </div>
+
+
+      <ul className="nav-links">
+
+        <li>
+          <a href="#home">Home</a>
+        </li>
+
+        <li>
+          <a href="#about">About</a>
+        </li>
+
+        <li>
+          <a href="#services">Services</a>
+        </li>
+
+        <li>
+          <a href="#skills">Skills</a>
+        </li>
+
+        <li>
+          <a href="#projects">Projects</a>
+        </li>
+
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
+
+      </ul>
+
+
+    </nav>
+  );
+}
+
+
+export default Navbar;
