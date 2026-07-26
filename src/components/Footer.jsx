d3ad901553194baeec54b1064b85c691a40490c2
essence100 <essencebenedict@gmail.com>
2026-07-27 00:47:07 +0300

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,1 +1,15 @@
+function Footer() {
 
+  return (
+    <footer className="footer">
+
+      <p>
+        © 2026 Benedict E. Charles | IT Specialist
+      </p>
+
+    </footer>
+  );
+}
+
+
+export default Footer;
